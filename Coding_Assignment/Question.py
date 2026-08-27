7585dc9fb40f9dc4f78a5effb47c01dd1a28ad72
9ahmedheshmat0-blip <9ahmedheshmat0@gmail.com>
2026-08-27 15:55:17 +0300

    Add tailgating detection function
    Implement tailgating detection logic to calculate incidents and minimum distance.

--- Coding_Assignment/Question.py
+++ Coding_Assignment/Question.py
@@ -13,3 +13,25 @@ def monitor_following_distance(distances: list[float], speeds: list[float]) -> t
             - tailgate_incidents: number of separate instances the car started tailgating
     """
     pass
+distance = [30.0 , 20.0 , 20.0 , 30.0 , 15.0, 10.0]
+speed = [50.0 , 50.0 , 50.0 , 50.0 , 50.0, 50.0]
+global tailg_inci , tailg_sec , min_distance, first
+first = True
+tailg_sec= 0
+min_distance = 0
+tailg_inci = 0
+def x(distance, speed):
+    global tailg_inci , tailg_sec , min_distance, first
+    for i , s in zip(distance, speed):
+        safe_distance = 0.5 * s
+        if i < safe_distance:
+            tailg_sec += 1
+            if first:
+                min_distance = i
+                first = False
+                tailg_inci += 1
+            if min_distance > i:
+                min_distance = i
+                tailg_inci += 1
+    print(tailg_inci, tailg_sec, min_distance)
+x(distance, speed)
